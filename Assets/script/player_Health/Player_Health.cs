d2cadfc5bd36d69c7507343376f0d56a0c712929
Léo Simon <leoalex.simon@gmail.com>
2024-03-16 15:54:37 +0100

    début de system de pv

---
+++ Assets/script/player_Health/Player_Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour
+{
+    [SerializeField] float health = 3;
+
+
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
