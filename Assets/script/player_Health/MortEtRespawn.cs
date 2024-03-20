e20c68d47fc23292a39992613621b85f7a845318
LéoSimon <leoalex.simon@gmail.com>
2024-03-20 14:07:39 +0100

    début respawn

---
+++ Assets/script/player_Health/MortEtRespawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MortEtRespawn : MonoBehaviour
+{
+    private Vector3 Respawn;
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("CheckPoint"))
+        {
+
+            transform.position = Respawn;
+
+        }
+    }
+
+
+
+}
