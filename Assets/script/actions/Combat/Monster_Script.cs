7fde25b2043c5da19487927d0e21fd7d5e0e5835
LéoSimon <leoalex.simon@gmail.com>
2024-03-29 13:59:34 +0100

    boulot sur le monstre anim mort marche pas ????

--- Assets/script/actions/Combat/Monster_Script.cs
+++ Assets/script/actions/Combat/Monster_Script.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class Monsters_SCript : MonoBehaviour
 {
     [SerializeField] float HitCheckRadius = 0.65f;
+    [SerializeField] float AttackCheckRadius = 0.65f;
     [SerializeField] Transform HitCheck;
+    [SerializeField] Transform AttackHox;
     [SerializeField] float MonsterHp = 2;
     public GameObject Monster;
     [SerializeField] LayerMask CollisionsLayers;
+    [SerializeField] LayerMask CollisionsLayersAttack;
     [SerializeField] bool isHit = false;
     [SerializeField] bool recoveryTime = false;
+    [SerializeField] Animator Animator_Monster;
+    [SerializeField] bool DoHit;
 
     //methode to make the monster lose Hp
     public void loseHp()
     {
+        if ( MonsterHp>0)
+        {
+            Animator_Monster.SetBool("BoolHurt", true);
+            MonsterHp--;
+            Debug.Log("Je perd un Pv");
+            recoveryTime = true;
+        }
 
-        MonsterHp--;
-        Debug.Log("Je perd un Pv");
-        recoveryTime = true;
+
+
+    }
+    public void finHurt() 
+    {
+
+        Animator_Monster.SetBool("BoolHurt", false);
+
 
     }
     // recovery time to prevent os of the monsters
@@ -27,6 +44,8 @@ public class Monsters_SCript : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
         recoveryTime = false;
+        finHurt();
+
 
 
     }
@@ -35,11 +54,16 @@ public class Monsters_SCript : MonoBehaviour
     {
         if (MonsterHp <= 0)
         {
-            Monster.SetActive(false);
+            Animator_Monster.SetBool("BoolDeath",true);
             Debug.Log("Je meurt !!");
+          //  Monster.SetActive(false);
 
         }
 
+    }
+    public void destroyed()
+    {
+        Monster.SetActive(false);
     }
 // methode that combine the loss of Hp and the recovery time
     void isHitted()
@@ -52,7 +76,36 @@ public class Monsters_SCript : MonoBehaviour
         }
 
     }
+    private void attack()
+    {
 
+        if (DoHit)
+        {
+
+
+            Animator_Monster.SetBool("BoolAttack", true);
+
+
+
+        }
+        else
+        {
+            Animator_Monster.SetBool("BoolAttack", false);
+
+
+        }
+
+
+
+    }
+
+    public void finAttack()
+    {
+
+        //Animator_Monster.SetBool("BoolAttack", false);
+
+
+    }
 
 
 
@@ -67,8 +120,10 @@ public class Monsters_SCript : MonoBehaviour
     {
         // "hitbox" of the monster
         isHit = Physics2D.OverlapCircle(HitCheck.position, HitCheckRadius, CollisionsLayers);
+        DoHit = Physics2D.OverlapCircle(AttackHox.position, AttackCheckRadius, CollisionsLayersAttack);
         death();
         isHitted();
+        attack();
 
 
     }
