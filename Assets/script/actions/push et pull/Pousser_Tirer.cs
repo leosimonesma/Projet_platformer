d3b51ed22793c3a54f861f20d5de442032fa5f4a
Léo Simon <leoalex.simon@gmail.com>
2024-05-07 12:43:58 +0200

    fix pousser tirer pour build + check coroutine attack

--- Assets/script/actions/push et pull/Pousser_Tirer.cs
+++ Assets/script/actions/push et pull/Pousser_Tirer.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class Pousser_Tirer : MonoBehaviour
